09bed9d4e42cacf0405c2ae0f1993653da649f30
Zac Stanton <zac71113@gmail.com>
2021-02-21 19:40:44 -0700

    EStopInterruptFatal and EStopInterrupt implemented
    provides functionality for fatal interrupts requiring computer shutdown vs interrupts which should stop the programs but leave the computer running

--- Robot/Robot.py
+++ Robot/Robot.py
@@ -1,9 +1,14 @@
 import os
-from threading import Thread, Lock
+import sys
+import time
+import traceback
+from threading import Thread
+from pynput import keyboard
 from pynput.keyboard import Listener, Key
 
-# Creation of EStop Exception
-class EStopInterrupt(Exception): args: True
+# Creation of EStop Exception and its Fatal counterpart (Fatal EStop completely shuts down computer)
+class EStopInterruptFatal(Exception): args: True 
+class EStopInterrupt(Exception): args: False
 
 parts = [] # Component list for ease of looping in autoOp/teleOp function
 operatingMode = True # Operate in Autonomous or TeleOp? True = TeleOp, False = Auto
@@ -11,44 +16,60 @@ operatingMode = True # Operate in Autonomous or TeleOp? True = TeleOp, False = A
 # Filling components
 # parts.append() ...
 
-
-
 # Defining stop method (Includes E Stop functionality through SHUTDOWN bool)
-def stop(SHUTDOWN = False):
+def stop(FATAL = False):
     for Comp in parts:
         Comp.kill()
     # If EStop was triggered, shutdown Jetson immediately
-    if(SHUTDOWN):
+    if(FATAL):
         print("Triggering system shutdown due to EStop")
         #os.system('shutdown /s /t 1')
     pass
 
-# Key listener
+# Key listener (used for interrupts that arent ctrl+c)
 def on_press(key):
-    if(key == 'q'):
+    if(key == Key.enter):
+        raise EStopInterruptFatal
+    if(key == Key.space):
         raise EStopInterrupt
 
 # Setup keyboard listener for EStop
 def KeyListener():
-    with Listener() as listener:
-        listener.join()
+    try:
+        with Listener(on_press=on_press, on_release=None) as listener:
+            listener.join()
+    except(EStopInterruptFatal, EStopInterrupt) as e:
+        # Adding traceback to file in ./Logs/ directory before deciding if to force shutdown
+        SYSTIME = time.gmtime(time.time())
+        SYSTIME = str(SYSTIME.tm_mday) + "_" + str(SYSTIME.tm_hour) + "_" + str(SYSTIME.tm_min) + "_" + str(SYSTIME.tm_sec) + "_Traceback"
+        f = open("Robot/Logs/" + f"{SYSTIME}" + ".txt", "w")
+        f.write(traceback.format_exc())
+        f.close()
 
-t = Thread(target=KeyListener)
-t.start()
+        # Shutdown NOW
+        stop(e.args)
 
-try:
+def start():
     # TODO CONSIDER ADDING TICKRATE HERE?????
-    while True:
+    while False:
         # Update each component of the robot depending on the operating mode
         if(operatingMode):
             for Comp in parts:
                 Comp.Update()
-            print("Hello from update")
         else:
             for Comp in parts:
                 Comp.autoUpdate()
+
+try:
+    # Thread for keyboard listener (EStop etc)
+    keyboardThread = Thread(target=KeyListener)
+    keyboardThread.start()
+
+    # Thread for actually doing things
+    functionalThread = Thread(target = start)
+    functionalThread.start()
 # If keyboard interrupt, shut down every single part
-except (KeyboardInterrupt, EStopInterrupt) as e:
-    stop(e.args)
+except (KeyboardInterrupt):
+    stop()
     
 
