102e808b64ece343b286eada6e82a884165a72ae
Zac Stanton <zac71113@gmail.com>
2021-03-04 20:30:45 -0700

    Remove \n from last line of Log Output

--- ROV/__main__.py
+++ ROV/__main__.py
@@ -38,10 +38,10 @@ def stop(FATAL = False):
         Comp.kill()
     # If EStop was triggered, shutdown Jetson immediately
     if(FATAL):
-        ROVMap.LOGGER.log("Triggering system shutdown due to EStop")
+        ROVMap.LOGGER.log("Triggering system shutdown due to EStop", end="")
         ROVMap.LOGGER.closeLog()
         #os.system('shutdown /s /t 1')
-    ROVMap.LOGGER.log("ROV Successfully Shutdown....Closing Log")
+    ROVMap.LOGGER.log("ROV Successfully Shutdown....Closing Log", end="")
     ROVMap.Logger.closeLog()
 
 # TODO: IMPLEMENT
