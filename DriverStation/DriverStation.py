05755843e4f3382847fe803025d1d21e227b2b68
Zac Stanton <zac71113@gmail.com>
2021-03-07 21:13:45 -0700

    xbox drivers added driver station started

---
+++ DriverStation/DriverStation.py
@@ -0,0 +1,15 @@
+# Code for Driver Station (Raspi running PiOS/CentOS/Ubuntu)
+
+from dearpygui.core import *
+from dearpygui.simple import *
+
+def save_callback(sender, data):
+    print("Save Clicked")
+
+with window("Example Window"):
+    add_text("Hello, world")
+    add_button("Save", callback=save_callback)
+    add_input_text("string", default_value="Quick brown fox")
+    add_slider_float("float", default_value=0.273, max_value=1)
+
+start_dearpygui()
