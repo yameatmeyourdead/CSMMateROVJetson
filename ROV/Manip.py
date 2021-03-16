f04d3a40aa99b8120bb880ecc3739d56d1e53c98
Zac Stanton <zac71113@gmail.com>
2021-03-16 15:27:32 -0600

    begin servo test

--- ROV/Manip.py
+++ ROV/Manip.py
@@ -1,3 +1,4 @@
+from time import sleep
 from .Component import Component
 from . import ROVMap
 
@@ -5,9 +6,14 @@ class Manip(Component):
     def __init__(self):
         # Do setup things
         Manip.logEvent("MANIPULATOR CONSTRUCTED")
+        # Try to set servo to specific angle
+        ROVMap.kit.servo[ROVMap.PCA9685PINOUT["MANIP_PLACEHOLDER_SERVO1"]].angle=137
 
     def Update(self):
+        sleep(5)
         print("Manipulator Update")
+        ROVMap.kit.servo[ROVMap.PCA9685PINOUT["MANIP_PLACEHOLDER_SERVO1"]].angle=100
+        sleep(5)
     
     def autoUpdate(self):
         print("Manipulator autoUpdate")
--- ROV/ROVMap.py
+++ ROV/ROVMap.py
@@ -1,10 +1,17 @@
+from .xbox import Joystick
 from .Logger import Logger
+from adafruit_servokit import ServoKit
 
 #use this to create constants/lists of constants for all needs
 
 LOGGER = Logger()
+CONTROLLER = Joystick()
 
-#Constant dictionary for PCA9685 (e.g key = SERVO1 and value = pinout of the pins responsible) 4 manip 3 micro 
+# PCA9685 should be connected to jetson on J41 pins 27(SDA)/28(SCL) for Bus0 or pins 3(SDA)/5(SCL) for Bus1 as well as relevant voltage (pin1/7 i think (CHECK CHECK CHECK))
+# Run PCA9685 at v+ = 5V 4A
+# Default I2C address is 0x40
+# Constant dictionary for PCA9685 (e.g key = SERVO1 and value = pinout of the pins responsible) 4 manip 3 micro 
+kit = ServoKit(channels=16)
 PCA9685PINOUT = {"FRONT_LEFT_THRUSTER_ESC": 0, "FRONT_RIGHT_THRUSTER_ESC": 1, "BACK_LEFT_THRUSTER_ESC": 2, "BACK_RIGHT_THRUSTER_ESC": 3, 
                 "MANIP_PLACEHOLDER_SERVO1": 4, "MANIP_PLACEHOLDER_SERVO2": 5, "MANIP_PLACEHOLDER_SERVO3": 6, "MANIP_PLACEHOLDER_SERVO4": 7,
                 "MICRO_PLACEHOLDER_SERVO1": 8, "MICRO_PLACEHOLDER_SERVO2": 9, "MICRO_PLACEHOLDER_SERVO3": 10, "MICRO_PLACEHOLDER_DCMOTORESC": 11}
