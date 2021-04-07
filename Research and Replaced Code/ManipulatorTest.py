e2c1742aaec1e1fb11143da23dd9fb40c31a4fcb
Yamea <zac71113@gmail.com>
2021-04-06 20:27:45 -0600

    Update ManipulatorTest.py

--- Research and Replaced Code/ManipulatorTest.py
+++ Research and Replaced Code/ManipulatorTest.py
@@ -110,8 +110,8 @@ y_velocity_old = 0
 y_velocity_tune = 0 # Tunes zeros of joystick
 global_velocity = 90
 
-DELTA_VELOCITY_IGNORE = .075 # Tunes how sensitive joystick is to changes
 VELOCITY_SCALING_FACTOR = .0125
+DELTA_VELOCITY_IGNORE = .075 * VELOCITY_SCALING_FACTOR # Tunes how sensitive joystick is to changes
 ELBOW_ANGLE_MAX = 180
 ELBOW_ANGLE_MIN = 0
 LEVEL_ANGLE_MAX = 180
