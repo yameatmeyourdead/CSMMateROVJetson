e65ce2e6e6911c22c6604824e1d8a744fb1493ee
Yamea <zac71113@gmail.com>
2021-04-08 19:11:26 -0600

    Update ThrusterTest.py

--- Research and Replaced Code/ThrusterTest.py
+++ Research and Replaced Code/ThrusterTest.py
@@ -7,10 +7,12 @@ kit.continuous_servo[0].set_pulse_width_range(1100,1900)
 go = True
 
 while True:
-    input("Toggle")
+    input("Toggle?")
     if(go):
         kit.continuous_servo[0].throttle = .25
         go = False
+        print("STOP")
     else:
         kit.continuous_servo[0].throttle = 0.0
+        print("GO")
         go = True
