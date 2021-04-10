f2caf4a5c994c3dd921185f708ce46ce3a4b1525
Yamea <zac71113@gmail.com>
2021-04-09 20:00:51 -0600

    Update NewThrusterTest.py

--- Research and Replaced Code/NewThrusterTest.py
+++ Research and Replaced Code/NewThrusterTest.py
@@ -8,7 +8,7 @@ kit = ServoKit(channels=16)
 # declare motor esc as continuous servo because it expects servo like PWM input
 kit._items[0] = servo.ContinuousServo(kit._pca.channels[0])
 ESC = kit._items[0]
-ESC.set_pulse_width_range(1100,1900)
+ESC.set_pulse_width_range(1100,2000)
 
 # allow myself to control it
 while True:
