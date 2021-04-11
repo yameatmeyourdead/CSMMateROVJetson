86a93001189670739a94f48d8661fa19b9b4a3e2
Yamea <zac71113@gmail.com>
2021-04-10 18:39:52 -0600

    try to fix crap

---
+++ Research and Replaced Code/FSTest/__main__.py
@@ -0,0 +1,2 @@
+from . import dotproducttest
+dotproducttest.start()
--- Research and Replaced Code/FSTest/dotproducttest.py
+++ Research and Replaced Code/FSTest/dotproducttest.py
@@ -3,13 +3,16 @@ from adafruit_motor import servo
 from .Vector import Vector
 from . import Controller
 
-kit = ServoKit(channels=16)
+SQRT2 = 1.4142136
 
-THRUSTER_FRONT_LEFT = kit._items[0] = servo.ContinuousServo(kit._pca.channels[0])
-THRUSTER_FRONT_LEFT_THRUST_VECTOR = Vector(Vector.SQRT2/2, Vector.SQRT2/2, 0)
+def start():
+    kit = ServoKit(channels=16)
 
-while True:
-    print("Input target velocity")
-    target = Vector(input("X> "), input("Y> "), input("Z> "))
-    throttle = THRUSTER_FRONT_LEFT_THRUST_VECTOR.dotProduct(target)
-    print(throttle)
+    THRUSTER_FRONT_LEFT = kit._items[0] = servo.ContinuousServo(kit._pca.channels[0])
+    THRUSTER_FRONT_LEFT_THRUST_VECTOR = Vector(SQRT2/2, SQRT2/2, 0)
+
+    while True:
+        print("Input target velocity")
+        target = Vector.Vector(input("X> "), input("Y> "), input("Z> "))
+        throttle = THRUSTER_FRONT_LEFT_THRUST_VECTOR.dotProduct(target)
+        print(throttle)
