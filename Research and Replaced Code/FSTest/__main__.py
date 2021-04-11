46d4e3107066a32ddc11cc5fd82e0eafb5a68d15
Yamea <zac71113@gmail.com>
2021-04-10 19:54:42 -0600

    Update __main__.py

--- Research and Replaced Code/FSTest/__main__.py
+++ Research and Replaced Code/FSTest/__main__.py
@@ -1,7 +1,7 @@
 from . import Translation
 from . import Azimuth
 
-choice = input("What would you like to test?\n1. Translation\n2. Azimuth Rotation")
+choice = int(input("What would you like to test?\n1. Translation\n2. Azimuth Rotation\n"))
 if(choice == 1):
     Translation.start()
 elif(choice == 2):
