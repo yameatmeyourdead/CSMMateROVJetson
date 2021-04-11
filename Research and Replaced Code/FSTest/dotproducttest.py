1ee05a524d5d08970fe5a01814a50264a1446428
Yamea <zac71113@gmail.com>
2021-04-10 19:00:00 -0600

    Update dotproducttest.py

--- Research and Replaced Code/FSTest/dotproducttest.py
+++ Research and Replaced Code/FSTest/dotproducttest.py
@@ -8,6 +8,8 @@ SQRT2 = 1.4142136
 def start():
     kit = ServoKit(channels=16)
 
+    VELOCITY_MOD = .75
+    
     THRUSTER_FRONT_LEFT = kit._items[0] = servo.ContinuousServo(kit._pca.channels[0])
     THRUSTER_FRONT_LEFT_THRUST_VECTOR = Vector(SQRT2/2, SQRT2/2, 0)
     THRUSTER_FRONT_RIGHT = kit._items[1] = servo.ContinuousServo(kit._pca.channels[1])
@@ -18,5 +20,5 @@ def start():
         poll = Controller.getRightStick()
         target = Vector(poll[0], poll[1], 0)
         print(target.toString())
-        THRUSTER_FRONT_LEFT.throttle = THRUSTER_FRONT_LEFT_THRUST_VECTOR.dotProduct(target)
-        THRUSTER_FRONT_RIGHT.throttle = -THRUSTER_FRONT_RIGHT_THRUST_VECTOR.dotProduct(target)
+        THRUSTER_FRONT_LEFT.throttle = THRUSTER_FRONT_LEFT_THRUST_VECTOR.dotProduct(target) * VELOCITY_MOD
+        THRUSTER_FRONT_RIGHT.throttle = -THRUSTER_FRONT_RIGHT_THRUST_VECTOR.dotProduct(target) * VELOCITY_MOD
