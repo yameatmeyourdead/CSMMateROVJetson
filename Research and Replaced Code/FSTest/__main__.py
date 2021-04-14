185b9e2dcb08767a346bbd332c4b7d8c1d72432b
Yamea <zac71113@gmail.com>
2021-04-14 00:47:02 -0600

    Update __main__.py

--- Research and Replaced Code/FSTest/__main__.py
+++ Research and Replaced Code/FSTest/__main__.py
@@ -2,8 +2,8 @@
 # from . import Azimuth
 from . import AllAxisDrive
 
-debug = bool(input("debug mode? 0/1"))
-AllAxisDrive.start(debug)
+#debug = bool(input("debug mode? 0/1"))
+AllAxisDrive.start(False)
 
 # choice = int(input("What would you like to test?\n1. Translation\n2. Azimuth Rotation\n"))
 # if(choice == 1):
