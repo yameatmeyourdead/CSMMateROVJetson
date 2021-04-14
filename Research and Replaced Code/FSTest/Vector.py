609b430f73f16f74d2131e425105f0d0bc5703bf
Zac Stanton <zac71113@gmail.com>
2021-04-14 00:17:18 -0600

    Update Vector.py

--- Research and Replaced Code/FSTest/Vector.py
+++ Research and Replaced Code/FSTest/Vector.py
@@ -52,6 +52,8 @@ class Vector:
         Convert Vector to its Unit Vector equivalent
         """
         magnitude = self.getMagnitude()
+        if(magnitude == 0):
+            return Vector()
         return Vector(self.getX()/magnitude, self.getY()/magnitude, self.getZ()/magnitude)
 
     def dotProduct(self, vector):
@@ -96,16 +98,3 @@ class Vector:
     
     def __truediv__(self, val):
         return Vector(self.getX() / val, self.getY() / val, self.getZ() / val)
-
-
-SQRT2 = 1.4142136
-
-if __name__ == "__main__":
-    Thruster1 = Vector(0,0,1)
-    Thruster2 = Vector(SQRT2/2,SQRT2/2,0)
-    Thruster3 = Vector(SQRT2/2,-SQRT2/2,0)
-    velocity = Vector(0,1,0)
-
-    print("Vector 1 dot product", Thruster1.dotProduct(velocity))
-    print("Thruster 2 dot product", Thruster2.dotProduct(velocity))
-    print("Thruster 3 dot product", -Thruster3.dotProduct(velocity))
