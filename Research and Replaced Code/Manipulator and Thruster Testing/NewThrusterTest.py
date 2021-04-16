1346b7ae4be1cc471daf164460ecbd8f7d249258
Yamea <zac71113@gmail.com>
2021-04-15 18:48:57 -0600

    Update NewThrusterTest.py

--- Research and Replaced Code/Manipulator and Thruster Testing/NewThrusterTest.py
+++ Research and Replaced Code/Manipulator and Thruster Testing/NewThrusterTest.py
@@ -6,8 +6,8 @@ kit = ServoKit(channels=16)
 
 
 # declare motor esc as continuous servo because it expects servo like PWM input
-kit._items[0] = servo.ContinuousServo(kit._pca.channels[0])
-ESC = kit._items[0]
+kit._items[4] = servo.ContinuousServo(kit._pca.channels[0])
+ESC = kit._items[4]
 
 # experimentally found pulse width range shifted -> 100
 ESC.set_pulse_width_range(1200,2000)
