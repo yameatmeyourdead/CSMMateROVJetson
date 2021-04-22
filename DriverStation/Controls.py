e33968be1e3b8df3629e25d84976eaa65566d9b2
Zac Stanton <zac71113@gmail.com>
2021-04-21 19:26:30 -0600

    Update Controls.py

--- DriverStation/Controls.py
+++ DriverStation/Controls.py
@@ -10,11 +10,7 @@ async def do_forward_device(i, device):
 		print(json.dumps([i, event.type, event.code, event.value]))
 
 async def forward_device(i, device):
-	if args.exclusive:
-		with device.grab_context():
-			await do_forward_device(i, device)
-	else:
-		await do_forward_device(i, device)
+	await do_forward_device(i, device)
 
 def encode_device(device):
 	cap = device.capabilities()
@@ -27,23 +23,21 @@ def encode_device(device):
 async def run_forward():
 	# Find devices
 	devices_by_name = {}
-	if args.device_by_name:
-		for path in evdev.list_devices():
-			device = evdev.InputDevice(path)
-			devices_by_name[device.name] = device
+	for path in evdev.list_devices():
+		device = evdev.InputDevice(path)
+		devices_by_name[device.name.lower()] = device
 	
 	devices = []
-	for path in args.device_by_path:
-		devices.append(evdev.InputDevice(path))
-	for name in args.device_by_name:
-		devices.append(devices_by_name[name])
+	devices.append(devices_by_name["Microsoft X-Box One S pad".lower()])
 	
 	
 	# Report devices
 	print(json.dumps([encode_device(device) for device in devices]))
 	
-	tasks = []
-	for i, device in enumerate(devices):
-		tasks.append(asyncio.create_task(forward_device(i, device)))
+	# tasks = []
+	# for i, device in enumerate(devices):
+		# tasks.append(asyncio.create_task(forward_device(i, device)))
 	
-	await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+	# await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+run_forward()
