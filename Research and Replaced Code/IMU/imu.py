d193deedfdcbad37c412a4917ab7ac2d076dd211
Yamea <zac71113@gmail.com>
2021-04-27 17:34:00 -0600

    Update imu.py

--- Research and Replaced Code/IMU/imu.py
+++ Research and Replaced Code/IMU/imu.py
@@ -8,7 +8,11 @@ NineAxisSensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
 
 while True:
-    print(f"Acceleration (m/s^2): {NineAxisSensor.acceleration}")
-    print(f"Magnetometer (gauss): {NineAxisSensor.magnetic}")
-    print(f"Gyro (deg/sec): {NineAxisSensor.gyro}")
+    accel_x, accel_y, accel_z = NineAxisSensor.acceleration
+    mag_x, mag_y, mag_z = NineAxisSensor.magnetic
+    gyro_x, gyro_y, gyro_z = NineAxisSensor.gyro
+
+    print(f"Acceleration (m/s^2): {accel_x}, {accel_y}, {accel_z}")
+    print(f"Magnetometer (gauss): {mag_x}, {mag_y}, {mag_z}")
+    print(f"Gyro (deg/sec): {gyro_x}, {gyro_y}, {gyro_z}")
     sleep(.5)
