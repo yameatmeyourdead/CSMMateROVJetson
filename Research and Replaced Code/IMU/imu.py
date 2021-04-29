fc2da2f13d745e8819f8bf430e5d7c5df0cd6308
Yamea <zac71113@gmail.com>
2021-04-28 18:49:45 -0600

    try accelerometer approximation of tilt

--- Research and Replaced Code/IMU/imu.py
+++ Research and Replaced Code/IMU/imu.py
@@ -2,10 +2,15 @@ from time import sleep
 import busio
 import board
 import adafruit_lsm9ds1
+import math
 from.Vector import Vector
 
 i2c = busio.I2C(board.SCL, board.SDA)
 NineAxisSensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+
+NineAxisSensor.gyro_scale = adafruit_lsm9ds1.GYROSCALE_245DPS
+NineAxisSensor.accel_range = adafruit_lsm9ds1.ACCELRANGE_2G
+NineAxisSensor.mag_gain = adafruit_lsm9ds1.MAGGAIN_4GAUSS
 
 class Quaternion:
     def __init__(self):
@@ -23,9 +28,18 @@ class Quaternion:
     def getZ(self):
         return self.__quaternion[3]
 
+theta = 0
+phi = 0
+
+tilt = 0
+
 while True:
     # update accelerometer, magnetometer, and gyroscope values
     
     accel = Vector.tupleToVector(NineAxisSensor.acceleration)
     gyro = Vector.tupleToVector(NineAxisSensor.gyro)
+    mag = Vector.tupleToVector(NineAxisSensor.magnetic)
     
+    tilt = math.atan(accel.getX()/accel.getZ())
+
+    print(tilt)
