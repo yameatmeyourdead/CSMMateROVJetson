e1ec0b4cf35686d64508142b1e9ac7aae9f1d69d
Yamea <zac71113@gmail.com>
2021-04-28 18:50:52 -0600

    Update imu.py

--- Research and Replaced Code/IMU/imu.py
+++ Research and Replaced Code/IMU/imu.py
@@ -3,7 +3,7 @@ import busio
 import board
 import adafruit_lsm9ds1
 import math
-from.Vector import Vector
+from .Vector import Vector
 
 i2c = busio.I2C(board.SCL, board.SDA)
 NineAxisSensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
