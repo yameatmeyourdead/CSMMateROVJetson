9b15570403799deef66bdcf8a9711cc462c136fd
Yamea <zac71113@gmail.com>
2021-05-13 13:41:15 -0600

    camera driver fixed

--- DriverStation/CameraDriver.py
+++ DriverStation/CameraDriver.py
@@ -19,19 +19,16 @@ def waitForImage():
     LABEL.pack()
     root.update()
 
-    temp_img = ImageTk.PhotoImage(Image.new("RGB", (800, 800)))
-
-    panel = Label(root, image=temp_img)
-    panel.image = temp_img
-    panel.pack(side="left", padx=10, pady=10)
-    root.update()
-
-    NULLFRAME = cv2.imread("DriverStation/Assets/NullFrame.jpg")
+    NULLFRAME = cv2.imread("./Assets/NullFrame.jpg")
 
     # initialize empty opencv frames so stitching them together works.
     frames = [NULLFRAME, NULLFRAME, NULLFRAME, NULLFRAME]
     organized_frames = [[frames[0], frames[1]], [frames[2], frames[3]]]  # man, naming variables is hard
-    output = concat_tile_resize(organized_frames)
+    output = ImageTk.PhotoImage(Image.fromarray(concat_tile_resize(organized_frames)))
+
+    panel = Label(root, image=output)
+    panel.pack(side="left", padx=10, pady=10)
+    root.update()
 
     # start looping over all the frames
     while True:
@@ -41,6 +38,7 @@ def waitForImage():
         # if recieved client did not specify camera designation error out
         if (len(clientName) <= 6 or not (0 <= int(clientName[6:]) <= 3)):
             # DSM.log("received image did not contain valid camera designation")
+            root.update()
             continue
 
         # grab camera designation
@@ -59,15 +57,8 @@ def waitForImage():
         if (len(frames) == 0):
             continue
 
-        # stitch the frames into one to be displayed
-        organized_frames = [[frames[0], frames[1]], [frames[2], frames[3]]]  # man, naming variables is hard
-        output = concat_tile_resize(organized_frames)
-        frame = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-        framePil = Image.fromarray(frame)
-        output = ImageTk.PhotoImage(framePil)
-
-        panel.configure(image=output)
-        panel.image = output
+        # stitch the frames into one to be displaye
+        panel.image = ImageTk.PhotoImage(Image.fromarray(concat_tile_resize([[frames[0], frames[1]], [frames[2], frames[3]]])))
 
         root.update()
 
@@ -101,20 +92,14 @@ class CameraDriver:
 # use this to put multiple images into one! expects a 2d list of images, and will stack accordingly
 def concat_tile_resize(list_2d):
     # stack first two cams horizontally (cam0|cam1)
-    firstTwo = cv2.hconcat(list_2d[0][0], list_2d[0][1])
+    firstTwo = cv2.hconcat([list_2d[0][0], list_2d[0][1]])
     # stack second two cams horizontally (cam2|cam3)
-    secondTwo = cv2.hconcat(list_2d[1][0], list_2d[1][1])
+    secondTwo = cv2.hconcat([list_2d[1][0], list_2d[1][1]])
     # combine them
     # (cam0|cam1)
     # (cam2|cam3)
-    return cv2.vconcat(firstTwo, secondTwo)
+    return cv2.vconcat([firstTwo, secondTwo])
 
 # helper method for concatenating picture tiles (use for grayscale images)
 def toColor(img):
     return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-if __name__ == "__main__":
-    cameraDriver = CameraDriver()
-    cameraDriver.start()
-    input()
-    cameraDriver.kill()
