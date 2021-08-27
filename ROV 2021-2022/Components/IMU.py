464a6f1d44346976510123907f87bb83127e7b34
Yamea <zac71113@gmail.com>
2021-08-27 14:44:43 -0600

    TODO: figure out how calibration is stored. added getters for gyro/accelerometer/magnetometer/linear acceleration

--- ROV 2021-2022/Components/IMU.py
+++ ROV 2021-2022/Components/IMU.py
@@ -1,3 +1,5 @@
+import os
+from typing import Tuple
 from Components import Component
 from tools import Logger
 import board
@@ -22,18 +24,52 @@ class IMU(Component):
     def getQuaternion(self):
         return self.BNO055.quaternion
     
+    def getGyroscope(self):
+        return self.BNO055.gyro
+
+    def getAccelerometer(self):
+        return self.BNO055.acceleration
+
+    def getMagnetometer(self):
+        return self.BNO055.magnetic
+
+    def getLinearAcceleration(self):
+        return self.BNO055.linear_acceleration
+
     def getGravity(self):
         return self.BNO055.gravity
     
     def getCalibrationStatus(self):
         return self.BNO055.calibration_status
     
+    # TODO: FIND OUT HOW THIS WORKS IN THIS LIBRARY
     def saveCalibration(self) -> bool:
-        pass
-    
+        if(self.BNO055.calibrated):
+            for componentStatus in self.BNO055.calibration_status:
+                if componentStatus != 3:
+                    return False
+            with open("data\\BNO055_CALIBRATION_DATA.dat", 'w') as f:
+                status = (self.BNO055.offsets_accelerometer, self.BNO055.offsets_gyroscope)
+                f.write(str(status[0]) + '\n' + str(status[1]))
+            return True
+        else:
+            return False
+
+    # TODO: FIND OUT HOW THIS WORKS IN THIS LIBRARY    
     def loadCalibration(self) -> bool:
-        # self.BNO055.offsets_accelerometer
-        pass
+        if os.path.exists("data\\BNO055_CALIBRATION_DATA.dat"):
+            with open("data\\BNO055_CALIBRATION_DATA.dat", 'r') as f:
+                CALIBRATION_DATA = f.readlines()
+            if(CALIBRATION_DATA is not None):
+                self.setSensorOffsets(CALIBRATION_DATA)
+            else:
+                return False
+        else:
+            return False
+    
+    # def setSensorOffsets(self, offsets:Tuple[str]):
+    #     self.BNO055.offsets_accelerometer = offsets[0].replace('\n', '')
+    #     self.BNO055.offsets_gyroscope = offsets[1].replace('\n', '')
 
     def update(self) -> None:
         return
